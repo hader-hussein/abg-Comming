bfc195a6282aa4e91860d22ba46845e230c29b59
Hadeer <hadeer@eramint.com>
2021-09-28 05:57:02 -0700

    comming soon

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $('.logo-abg').addClass('animated fadeInLeft');
+    $('.logo').addClass('animated fadeInRight');
+    $('.Group').addClass('animated fadeInDown');
+
+    $('.text h1').addClass(' delay animated-wobble  wobble');
+});
